5eb96e61de2c67dab11d69ad5f9ae5bd09d12729
abhishek dulat <abhishekdulat@abhisheks-MacBook-Air.local>
2025-12-25 22:06:32 +0530

    copy

--- src/app/copyTrading/routes/copyTrading.routes.ts
+++ src/app/copyTrading/routes/copyTrading.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { requireAuth } from "../../../middleware/auth";
+import { requireAuth, Roles } from "../../../middleware/auth";
 import { CopyTradingController } from "../controller/copyTrading.controller";
 
 export class CopyTradingRouter {
@@ -13,23 +13,47 @@ export class CopyTradingRouter {
   init() {
     const controller = new CopyTradingController();
 
-    this.router.get("/master/me", requireAuth, controller.getMyMaster);
-    this.router.post("/master", requireAuth, controller.upsertMyMaster);
+    this.router.get(
+      "/master/me",
+      requireAuth([Roles.USER, Roles.ADMIN]),
+      controller.getMyMaster
+    );
+    this.router.post(
+      "/master",
+      requireAuth([Roles.USER, Roles.ADMIN]),
+      controller.upsertMyMaster
+    );
 
-    this.router.get("/masters", requireAuth, controller.listMasters);
+    this.router.get(
+      "/masters",
+      requireAuth([Roles.USER, Roles.ADMIN]),
+      controller.listMasters
+    );
 
-    this.router.post("/follow", requireAuth, controller.followMaster);
-    this.router.get("/follows/me", requireAuth, controller.listMyFollows);
+    this.router.post(
+      "/follow",
+      requireAuth([Roles.USER, Roles.ADMIN]),
+      controller.followMaster
+    );
+    this.router.get(
+      "/follows/me",
+      requireAuth([Roles.USER, Roles.ADMIN]),
+      controller.listMyFollows
+    );
     this.router.patch(
       "/follows/:followId",
-      requireAuth,
+      requireAuth([Roles.USER, Roles.ADMIN]),
       controller.updateMyFollow
     );
 
-    this.router.get("/followers/me", requireAuth, controller.listMyFollowers);
+    this.router.get(
+      "/followers/me",
+      requireAuth([Roles.USER, Roles.ADMIN]),
+      controller.listMyFollowers
+    );
     this.router.patch(
       "/followers/:followId/decision",
-      requireAuth,
+      requireAuth([Roles.USER, Roles.ADMIN]),
       controller.decideFollowerRequest
     );
   }
